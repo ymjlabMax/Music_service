f67730346bcc2b96ffd4a903ae0057e3455a91a3
Max <sud665@ymjlab.com>
2022-11-21 10:22:13 +0900

    forground service 구현

--- app/src/main/java/com/example/service_music/MusicService.java
+++ app/src/main/java/com/example/service_music/MusicService.java
@@ -1,14 +1,24 @@
 package com.example.service_music;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MusicService extends Service {
 
     private MediaPlayer player;
+
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
 
     public MusicService() {
     }
@@ -36,9 +46,45 @@ public class MusicService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId){
         Log.d("음악 호출", "onCreate()");
 
+        String input = intent.getStringExtra("inputExtra");
+
+        createNotificationChannel();
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("음악 어플 실행 ")
+                .setContentText("음악 재생 중 입니다.")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .build();
+
+
+        startForeground(1, notification);
+
+
+
         player = MediaPlayer.create(this, R.raw.music);
         player.setLooping(true);
         player.start();
         return super.onStartCommand(intent, flags, startId);
     }
+
+   private void createNotificationChannel(){
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+           NotificationChannel serviceChannel = new NotificationChannel(
+                   CHANNEL_ID,
+                   "Foreground Service Channel",
+                   NotificationManager.IMPORTANCE_DEFAULT
+           );
+
+           NotificationManager manager = getSystemService(NotificationManager.class);
+           assert manager != null;
+           manager.createNotificationChannel(serviceChannel);
+       }
+   }
+
+
+
+
 }
